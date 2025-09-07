798b7ad9cf597a53f29942caec2a87bcdf37fdd6
Nabath Nuur <muhammadnabathnuur@gmail.com>
2025-09-07 23:50:45 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
 
         SinglyLinkedList sll = new SinglyLinkedList();
 
-        System.out.println("Penambahan data");
+        System.out.println("\nPenambahan data");
         sll.addFirst("A1"); // [A1]   size = 1
         sll.addLast("B5"); // [A1, B5]   size = 2
         sll.insertBefore("B5", "T4"); // [A1, T4, B5]   size = 3
@@ -15,7 +15,7 @@ class Main {
         sll.insertAt(3, "C3"); // [A1, G2, P3, C3, T4, B5]   size = 6
         sll.insertAt(4, "I3"); // [A1, G2, P3, C3, I3, T4, B5]   size = 7
 
-        System.out.println("Menampilkan data");
+        System.out.println("\nMenampilkan data");
         sll.showForward(); // Data Singly Linkedlist urutan dari depan: A1, G2, P3, C3, I3, T4, B5,
 
         System.out.println("Penghapusan data");
@@ -24,10 +24,10 @@ class Main {
         sll.remove("G2"); // [P3, C3, I3, T4]   size = 4
         sll.removeAt(0); // [C3, I3, T4]   size = 3
 
-        System.out.println("Menampilkan data");
+        System.out.println("\nMenampilkan data");
         sll.showForward();  // Data Singly Linkedlist urutan dari depan: C3, I3, T4, 
 
-        System.out.println("Pengecekan data");
+        System.out.println("\nPengecekan data");
         System.out.println("\nIndex ke 1 adalah: " + sll.get(1)); // Index ke 1 adalah: I3
         System.out.println("Data C3 berada dalam list: " + sll.contains("C3")); // Data C3 berada dalam list: true
         System.out.println("Data T4 berada pada index ke: " + sll.indexOf("T4")); // Data T4 berada pada index ke: 2
@@ -37,7 +37,7 @@ class Main {
         
         DoublyLinkedList dll = new DoublyLinkedList();
 
-        System.out.println("Penambahan data");
+        System.out.println("\nPenambahan data");
         dll.addFirst("A1"); // [A1]   size = 1
         dll.addLast("B5"); // [A1, B5]   size = 2
         dll.insertBefore("B5", "T4"); // [A1, T4, B5]   size = 3
@@ -46,21 +46,21 @@ class Main {
         dll.insertAt(3, "C3"); // [A1, G2, P3, C3, T4, B5]   size = 6
         dll.insertAt(4, "I3"); // [A1, G2, P3, C3, I3, T4, B5]   size = 7
 
-        System.out.println("Menampilkan data");
+        System.out.println("\nMenampilkan data");
         dll.showForward(); // Data Doubly Linkedlist urutan dari depan: A1, G2, P3, C3, I3, T4, B5,
         dll.showBackward(); // Data Doubly Linkedlist urutan dari belakang: B5, T4, I3, C3, P3, G2, A1,
 
-        System.out.println("Penghapusan data");
+        System.out.println("\nPenghapusan data");
         dll.removeFirst();  // [G2, P3, C3, I3, T4, B5]   size = 6
         dll.removeLast(); // [G2, P3, C3, I3, T4]   size = 5
         dll.remove("G2"); // [P3, C3, I3, T4]   size = 4
         dll.removeAt(0); // [C3, I3, T4]   size = 3
 
-        System.out.println("Menampilkan data");
+        System.out.println("\nMenampilkan data");
         dll.showForward();  // Data Doubly Linkedlist urutan dari depan: C3, I3, T4, 
         dll.showBackward(); // Data Doubly Linkedlist urutan dari belakang: T4 , I3 , C3 ,
 
-        System.out.println("Pengecekan data");
+        System.out.println("\nPengecekan data");
         System.out.println("\nIndex ke 1 adalah: " + sll.get(1)); // Index ke 1 adalah: I3
         System.out.println("Data C3 berada dalam list: " + sll.contains("C3")); // Data C3 berada dalam list: true
         System.out.println("Data T4 berada pada index ke: " + sll.indexOf("T4")); // Data T4 berada pada index ke: 2
